60423b7346bdcb2b36d5735a5b98f1bc78d82c4d
chandru vijayakumar <chandru4395393@gmail.com>
2023-12-29 20:37:31 +0530

    task 5

--- demo.js
+++ demo.js
@@ -7,8 +7,7 @@ const myJson = {
     
   };
   // Using for loop
- 
-    console.log("Using for loop:");
+     console.log("Using for loop:");
     const keys = Object.keys(myJson);
     for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
